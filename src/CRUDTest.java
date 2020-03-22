17bcee4b4db9b23c99f555955812f39a505ad6b6
Miguel36 <miguelmontilla.1936@gmail.com>
2020-03-22 10:00:08 -0500

    Insert data using PreparedStatement

--- src/CRUDTest.java
+++ src/CRUDTest.java
@@ -1,5 +1,6 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.Statement;
 
 public class CRUDTest {
@@ -15,11 +16,18 @@ public class CRUDTest {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			Connection connection = DriverManager.getConnection(URL,USERNAME,PASSWORD);
 			
-			Statement statement = connection.createStatement();
-			//int result = statement.executeUpdate("CREATE TABLE Employee(emp_id INT, emp_name VARCHAR2(30), emp_salary INT)");
-			//int result = statement.executeUpdate("INSERT INTO Employee VALUES (103, 'EmployeeC', 30000)");
-			//int result = statement.executeUpdate("UPDATE Employee SET emp_salary = 15000 WHERE emp_id = 101");
-			int result = statement.executeUpdate("DELETE FROM Employee");
+//			Statement statement = connection.createStatement();
+//			int result = statement.executeUpdate("CREATE TABLE Employee(emp_id INT, emp_name VARCHAR2(30), emp_salary INT)");
+//			int result = statement.executeUpdate("INSERT INTO Employee VALUES (103, 'EmployeeC', 30000)");
+//			int result = statement.executeUpdate("UPDATE Employee SET emp_salary = 15000 WHERE emp_id = 101");
+//			int result = statement.executeUpdate("DELETE FROM Employee");
+			
+			
+			PreparedStatement pStatement = connection.prepareStatement("INSERT INTO Employee VALUES (?,?,?)");
+			pStatement.setInt(1, 103);
+			pStatement.setString(2, "EmployeeC");
+			pStatement.setInt(3, 30000);
+			int result = pStatement.executeUpdate();
 			
 			System.out.println("Number of rows affected: " + result);
 			connection.close();
