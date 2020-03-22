b1537d8e0490aebb07325084098de733358ab0f0
Miguel36 <miguelmontilla.1936@gmail.com>
2020-03-21 20:08:50 -0500

    Se crea el statement

--- src/JDBCTest.java
+++ src/JDBCTest.java
@@ -1,18 +1,21 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.Statement;
 
 public class JDBCTest {
 
     public static void main(String[] args){
 
         try {
-            System.out.println("Connecting to the database");
-
-            Class.forName("oracle.jdbc.driver.OracleDriver");
-            Connection connection = DriverManager.getConnection(
-                    "jdbc:oracle:thin:@localhost:1521:xe", "system", "12345");
-
-                System.out.println("Conection stablished!!!");
+        	System.out.println("Connecting to the database");
+	
+	        Class.forName("oracle.jdbc.driver.OracleDriver");
+	        Connection connection = DriverManager.getConnection(
+	                "jdbc:oracle:thin:@localhost:1521:xe", "system", "12345");
+	
+	        System.out.println("Conection stablished!!!");
+	        
+	        Statement statement = connection.createStatement();
             
         }
         catch (Exception e) {
